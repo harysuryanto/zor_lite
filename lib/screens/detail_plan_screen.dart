932aa1c07a7e3132648eac454545d67003023731
Hary Suryanto <hary.suryanto01@gmail.com>
2022-03-08 20:47:06 +0700

    add progress to detail plan screen

--- lib/screens/detail_plan_screen.dart
+++ lib/screens/detail_plan_screen.dart
@@ -1,8 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 import '../databases/database.dart';
+import '../models/exercise.dart';
+import '../widgets/exercise/exercise_list.dart';
 import '../widgets/global/ScaffoldBodyWithSafeArea/scaffold_body_with_safe_area.dart';
 
 class DetailPlanScreen extends StatelessWidget {
@@ -24,19 +27,32 @@ class DetailPlanScreen extends StatelessWidget {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         middle: Text(planName),
+        trailing: CupertinoButton(
+          padding: EdgeInsets.zero,
+          child: const Text('Add exercise'),
+          onPressed: () {
+            context.push('/add-exercise?planId=$planId&planName=$planName');
+          },
+        ),
         previousPageTitle: 'Home',
       ),
       child: ScaffoldBodyWithSafeArea(
         children: [
-          Text('planId: $planId'),
-          // LimitedBox(
-          //   maxHeight: 300,
-          //   child: StreamProvider<List<Exercise>>.value(
-          //     value: db.streamPlans(user!),
-          //     initialData: const [],
-          //     child: const ExerciseList(),
-          //   ),
-          // ),
+          const Text(
+            'Exercises',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          StreamProvider<List<Exercise>>.value(
+            value: db.streamExercises(user!, planId),
+            initialData: const [],
+            child: ExerciseList(
+              planId: planId,
+              shrinkWrap: true,
+            ),
+          ),
         ],
       ),
     );
