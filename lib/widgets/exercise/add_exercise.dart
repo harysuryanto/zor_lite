c4eed2e3a0d66e78c469566983b5bfa644e38264
Hary Suryanto <hary.suryanto01@gmail.com>
2022-03-24 21:39:23 +0700

    improve action sheet ui minor

--- lib/widgets/exercise/add_exercise.dart
+++ lib/widgets/exercise/add_exercise.dart
@@ -35,114 +35,141 @@ class _AddExerciseState extends State<AddExercise> {
     final db = DatabaseService();
     final user = Provider.of<User?>(context, listen: false);
 
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            _customContainer(
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Add exercise',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+    return SizedBox(
+      width: 600,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _customContainer(
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Add exercise',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 24),
-                    Form(
-                      key: formKey,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          /// Name
-                          const Text('Name'),
-                          const SizedBox(height: 8),
-                          CupertinoTextFormFieldRow(
-                            placeholder: 'E.g., Push up',
-                            keyboardType: TextInputType.name,
-                            textInputAction: TextInputAction.next,
-                            controller: exerciseName,
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please fill this input.';
-                              }
-                              return null;
-                            },
-                          ),
+                      const SizedBox(height: 24),
+                      Form(
+                        key: formKey,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            /// Name
+                            const Text('Name'),
+                            const SizedBox(height: 8),
+                            CupertinoTextFormFieldRow(
+                              placeholder: 'E.g., Push up',
+                              keyboardType: TextInputType.name,
+                              textInputAction: TextInputAction.next,
+                              controller: exerciseName,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please fill this input.';
+                                }
+                                return null;
+                              },
+                              decoration: const BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    width: 1,
+                                    color: CupertinoColors.systemGrey,
+                                  ),
+                                ),
+                              ),
+                            ),
 
-                          const SizedBox(height: 16),
+                            const SizedBox(height: 16),
 
-                          /// Repetitions
-                          const Text('Repetitions'),
-                          const SizedBox(height: 8),
-                          CupertinoTextFormFieldRow(
-                            controller: exerciseRepetitions,
-                            placeholder: 'E.g., 20',
-                            keyboardType: TextInputType.number,
-                            textInputAction: TextInputAction.next,
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please fill this input.';
-                              }
-                              return null;
-                            },
-                          ),
+                            /// Repetitions
+                            const Text('Repetitions'),
+                            const SizedBox(height: 8),
+                            CupertinoTextFormFieldRow(
+                              controller: exerciseRepetitions,
+                              placeholder: 'E.g., 20',
+                              keyboardType: TextInputType.number,
+                              textInputAction: TextInputAction.next,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please fill this input.';
+                                }
+                                return null;
+                              },
+                              decoration: const BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    width: 1,
+                                    color: CupertinoColors.systemGrey,
+                                  ),
+                                ),
+                              ),
+                            ),
 
-                          const SizedBox(height: 16),
+                            const SizedBox(height: 16),
 
-                          /// Sets
-                          const Text('Sets'),
-                          const SizedBox(height: 8),
-                          CupertinoTextFormFieldRow(
-                            controller: exerciseSets,
-                            placeholder: 'E.g., 4',
-                            keyboardType: TextInputType.number,
-                            textInputAction: TextInputAction.done,
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please fill this input.';
-                              }
-                              return null;
-                            },
-                          ),
-                        ],
+                            /// Sets
+                            const Text('Sets'),
+                            const SizedBox(height: 8),
+                            CupertinoTextFormFieldRow(
+                              controller: exerciseSets,
+                              placeholder: 'E.g., 4',
+                              keyboardType: TextInputType.number,
+                              textInputAction: TextInputAction.done,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please fill this input.';
+                                }
+                                return null;
+                              },
+                              decoration: const BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    width: 1,
+                                    color: CupertinoColors.systemGrey,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 12),
-            _customContainer(
-              child: CupertinoButton(
-                child: const Text('Save'),
-                onPressed: () {
-                  if (formKey.currentState!.validate()) {
-                    db.addExercise(
-                      user!,
-                      widget.planId,
-                      {
-                        'name': exerciseName.text.trim(),
-                        'repetitions':
-                            int.tryParse(exerciseRepetitions.text.trim()),
-                        'sets': int.tryParse(exerciseSets.text.trim()),
-                      },
-                    );
-                    Navigator.pop(context);
-                  }
-                },
+              const SizedBox(height: 12),
+              _customContainer(
+                child: CupertinoButton(
+                  child: const Text('Save'),
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      db.addExercise(
+                        user!,
+                        widget.planId,
+                        {
+                          'name': exerciseName.text.trim(),
+                          'repetitions':
+                              int.tryParse(exerciseRepetitions.text.trim()),
+                          'sets': int.tryParse(exerciseSets.text.trim()),
+                        },
+                      );
+                      Navigator.pop(context);
+                    }
+                  },
+                ),
               ),
-            ),
 
-            /// Padding bottom to prevent content blocked by keyboard
-            SizedBox(height: MediaQuery.of(context).viewInsets.bottom),
-          ],
+              /// Padding bottom to prevent content blocked by keyboard
+              SizedBox(height: MediaQuery.of(context).viewInsets.bottom),
+            ],
+          ),
         ),
       ),
     );
@@ -151,7 +178,7 @@ class _AddExerciseState extends State<AddExercise> {
   Container _customContainer({required Widget child}) {
     final themeBrightness = CupertinoTheme.brightnessOf(context);
     final backgroundColor = themeBrightness == Brightness.dark
-        ? CupertinoColors.black
+        ? CupertinoColors.darkBackgroundGray
         : CupertinoColors.white;
 
     return Container(
